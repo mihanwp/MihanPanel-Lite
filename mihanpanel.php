2c59b39b2bc4497902e88667e14046eb3d53a401
Hossein Kamrani <h.kamrani.dev@gmail.com>
2021-07-12 17:15:38 +0430

    change version: 9.4

--- mihanpanel.php
+++ mihanpanel.php
@@ -4,7 +4,7 @@ Plugin Name: MihanPanel
 Plugin URI:  https://mihanwp.com/mihanpanel/
 Author:      MihanWP
 Author URI:  https://mihanwp.com
-Version:     9.3.1
+Version:     9.4
 Description: The best way to build user interface in WordPress
 */
 //if accessed direct
